008cb131d0c8c7b1c23223c0c1b23f35f2cae6d0
KANISHKAJOSHI41 <joshikanishka24605@gmail.com>
2024-07-25 16:04:48 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,16 @@
+class variable{
+    public static void main(String[] args) {
+        
+        int num=20;
+        short b= 9;
+        long x= 12435;
+        byte v=-120;
+
+        System.out.println(num);
+        System.out.println(b);
+        System.out.println(x);
+        System.out.println(v);
+        
+        
+    }
+}
