4b7a5e9db8b28ad2d2541d66814772f0570fd361
KANISHKAJOSHI41 <joshikanishka24605@gmail.com>
2024-08-02 17:08:48 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,14 @@
+public class fact {
+    public static void main(String[] args) {
+        int b=6;
+        int fact=1;
+        for(int i=1;i<=b;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+
+}
+    
+
